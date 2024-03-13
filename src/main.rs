b8a4d00b2ada69fddbfaf4836fda4a66e2620f25
Muhammad Harris <harris.perceptron@gmail.com>
2024-03-13 06:38:22 +0500

    feat: bullet added

--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,18 @@ use crossterm::{
 use invader::sized_vector;
 use std::io::Write;
 
-struct Game {
-    score: usize,
+pub struct Game {
+    score: f32,
     lives: usize,
+}
+
+impl Default for Game {
+    fn default() -> Self {
+        Self {
+            score: 0.0,
+            lives: 3,
+        }
+    }
 }
 
 pub struct GameBuffer {
@@ -26,10 +35,14 @@ pub struct GameBuffer {
 
     // x1, y1, x2, y2
     boundary_coordinates: (usize, usize, usize, usize),
+
+    last_bullet_location: Option<(usize, usize)>,
+
+    bullet_speed: usize,
 }
 
 impl GameBuffer {
-    pub fn new() -> GameBuffer {
+    pub fn new(game: &Game) -> GameBuffer {
         let mut game_buffer = GameBuffer {
             grid: vec![],
             rows: 32,
@@ -40,9 +53,11 @@ impl GameBuffer {
 
             ship_length: 3,
             boundary_coordinates: (2, 2, 62, 30),
+            last_bullet_location: None,
+            bullet_speed: 1,
         };
 
-        game_buffer.init();
+        game_buffer.init(game);
 
         game_buffer
     }
@@ -102,34 +117,17 @@ impl GameBuffer {
             self.grid.push(row);
         }
     }
-    fn init(&mut self) {
+    fn init(&mut self, game: &Game) {
         self.init_buffer();
         self.clear();
 
+        self.draw_text(game);
         self.draw_boundary();
         let (x1, y1, _, _) = self.boundary_coordinates;
 
         self.draw_enemy(y1 + 1, 10);
         self.draw_ship(0);
-
-        //self.draw();
     }
-
-    //fn clear_screen(&self) {
-    //    print!("\x1B[2J\x1B[1;1H");
-    //    std::io::stdout().flush().unwrap()
-    //}
-    //pub fn draw(&self) {
-    //    self.clear_screen();
-
-    //    for i in &self.grid {
-    //        for j in i {
-    //            print!("{}", j);
-    //        }
-    //        println!();
-    //        std::io::stdout().flush().unwrap();
-    //    }
-    //}
 
     pub fn get_cols(&self) -> usize {
         self.cols
@@ -166,6 +164,106 @@ impl GameBuffer {
                 //    item.clear();
                 //    item.insert(0, '|');
                 //}
+            }
+        }
+    }
+
+    pub fn draw_text(&mut self, game: &Game) {
+        let score_text = format!("Score: {}", game.score);
+        let lives_text = format!("Lives: {}", game.lives);
+
+        let score_text_len = score_text.len();
+        let lives_text_len = lives_text.len();
+
+        let total_len = score_text_len + lives_text_len;
+
+        if total_len > self.cols {
+            panic!(
+                "Text length exceeds total buffer cols: {}>{}",
+                total_len, self.cols
+            );
+        }
+
+        let space_len = self.cols - total_len;
+
+        let mut game_display_text = String::new();
+        game_display_text += score_text.as_str();
+
+        for _ in 0..space_len {
+            game_display_text += " ";
+        }
+
+        game_display_text += lives_text.as_str();
+
+        let display_row_index = self.boundary_coordinates.1 - 1;
+        let display_row = &mut self.grid[display_row_index];
+
+        assert!(
+            game_display_text.len() <= display_row.len(),
+            "display text length greater than total buffer cols"
+        );
+
+        for (i, ch) in game_display_text.chars().enumerate() {
+            let col = &mut display_row[i];
+            col.clear();
+            col.insert(0, ch);
+        }
+    }
+
+    pub fn draw_bullet(&mut self, mut x: usize, mut y: usize) {
+        if let Some((x_prev, y_prev)) = self.last_bullet_location {
+            if x != x_prev {
+                return;
+            }
+
+            let bullet = &mut self.grid[y_prev][x_prev];
+            bullet.clear();
+            bullet.insert(0, ' ');
+        }
+
+        let (x1, y1, x2, y2) = self.boundary_coordinates;
+
+        //bullet should remain within game boundaries
+        if y <= y1 {
+            y = y1 + 1;
+        }
+
+        if y >= y2 {
+            y = y2 - 2;
+        }
+
+        if x <= x1 {
+            x = x1 + 1;
+        }
+
+        if x >= x2 {
+            x = x2 - 1;
+        }
+
+        let pixel = &mut self.grid[y][x];
+        pixel.clear();
+        pixel.insert(0, '|');
+
+        self.last_bullet_location = Some((x, y));
+    }
+
+    fn clear_bullet(&mut self) {
+        if let Some((x, y)) = self.last_bullet_location {
+            let item = &mut self.grid[y][x];
+
+            item.clear();
+            item.insert(0, ' ');
+        }
+    }
+
+    pub fn bullet_progress(&mut self) {
+        if let Some((x, y)) = self.last_bullet_location {
+            let new_y = y - self.bullet_speed;
+
+            if new_y <= self.boundary_coordinates.1 {
+                self.clear_bullet();
+            } else {
+                self.draw_bullet(x, new_y);
             }
         }
     }
@@ -276,12 +374,16 @@ impl Drop for KeyboardHandler {
 fn main() -> Result<()> {
     println!("Hello, world!");
 
-    let mut gb = GameBuffer::new();
+    let game = Game::default();
+    let mut gb = GameBuffer::new(&game);
 
     let mut key_handler = KeyboardHandler::new();
 
+    gb.draw_bullet(10, 100);
     loop {
         let key_event = key_handler.handle(&mut gb);
+
+        gb.bullet_progress();
 
         let tr = TerminalRenderer::new(&gb);
         tr.draw()?;
@@ -291,7 +393,7 @@ fn main() -> Result<()> {
                 break;
             }
         }
-        std::thread::sleep(std::time::Duration::from_millis(10));
+        std::thread::sleep(std::time::Duration::from_millis(100));
     }
 
     Ok(())
